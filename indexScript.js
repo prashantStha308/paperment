8b2c667e131239d579e7d6240bb5fba15c4a4616
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2024-08-06 21:13:30 +0545

    updated image assets

--- indexScript.js
+++ indexScript.js
@@ -3,14 +3,14 @@ const shopSection1 = document.getElementById("shopSection1"); //The first shop s
 const shopSection1Elements = document.getElementsByClassName("shopElements"); //Array of elements of class "shopSection1Elements"
 const shopHeading = document.getElementById("sectionHeading");
 
+// Generating grid areas for shop sections elements
 let i = 1;
 Array.from(shopSection1Elements).forEach( element=>{
     element.style.gridArea = `sectionElement${i}`;
     i++;
 } );
 
-console.log(shopSection1Elements);
-
+// Generating grid areas for shop headings
 let j = 1;
 Array.from(shopHeading).forEach( element=>{
     element.style.gridArea = `shopHeading${j}`;
