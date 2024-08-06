6583ec40244eb5cecb2f6e069f729ef9b44815d3
Prashant Shrestha <161279461+prashantStha308@users.noreply.github.com>
2024-08-07 00:18:39 +0545

    minor fixes. Added a new directory for new page

--- globalScript.js
+++ globalScript.js
@@ -1,38 +1,3 @@
 /* Script here will effect all the existing html files*/
 'use strict'
 
-//Start of Header Scroll animation section
-var didScroll; //boolean to determine if the user has scrolled or not
-var lastScrollTop = 0; //last time where the scroll ended
-var delta = 10; // to prevent 'accidental' trigger
-var navbarHeight = 85; // height of the element. 85 is a guessed number
-var navbarElem = document.getElementById("header"); //getting the header element
-
-window.addEventListener('scroll', ()=> {
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 300); // debounce
-
-function hasScrolled() {
-    var st = window.scrollY; //captures how much the user has scrolled
-
-    if (Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    if ( st > navbarHeight) {
-        navbarElem.classList.add('beActive');
-        navbarElem.style.transition = "all 0.3s ease-out";
-    } else {
-        if(st - window.innerHeight < window.outerHeight) {
-            navbarElem.classList.remove('beActive');
-        }
-    }
-    lastScrollTop = st;
-}
-//End of Header Scroll animation section
