bb664d34ad245e438a6694f4f2f335e752fa4ddd
prashantStha308 <prashant.795424@cct.tu.edu.np>
2024-08-15 10:00:34 +0545

    completed the home page and minor changes to file names

--- art/artScript.js
+++ art/artScript.js
@@ -38,58 +38,58 @@ function sortArray(array, property) {
 
 function createTile( product , container ){
 
-        // creating tile
-        const tile = document.createElement("div");
-        tile.classList.add("elementContainer");
+    // creating tile
+    const tile = document.createElement("div");
+    tile.classList.add("elementContainer");
 
-        // creating tile image
-        const tileImage = document.createElement("div");
-        tileImage.classList.add("elementImg");
-        tileImage.style.backgroundImage = `url( ${product.imgSrc} )` ;
+    // creating tile image
+    const tileImage = document.createElement("div");
+    tileImage.classList.add("elementImg");
+    tileImage.style.backgroundImage = `url( ${product.imgSrc} )` ;
 
-        // creating tile text Container
-        const tileTextContainer = document.createElement("div");
-        tileTextContainer.classList.add("elementTextContainer");
+    // creating tile text Container
+    const tileTextContainer = document.createElement("div");
+    tileTextContainer.classList.add("elementTextContainer");
 
-        // creating the text section
-        const tileText = document.createElement("p");
-        tileText.classList.add("elementText");
+    // creating the text section
+    const tileText = document.createElement("p");
+    tileText.classList.add("elementText");
 
-        // creating a span that will contain the name of the company
-        const tileName = document.createElement("span");
-        tileName.classList.add("elementName");
-        tileName.textContent = product.name;
+    // creating a span that will contain the name of the company
+    const tileName = document.createElement("span");
+    tileName.classList.add("elementName");
+    tileName.textContent = product.name;
 
-        // creating a span element that'll contain the price of the product
-        const tilePrice = document.createElement("span");
-        tilePrice.classList.add("elementPrice");
-        tilePrice.innerHTML = ` <br> Rs.${product.price}`;
+    // creating a span element that'll contain the price of the product
+    const tilePrice = document.createElement("span");
+    tilePrice.classList.add("elementPrice");
+    tilePrice.innerHTML = ` <br> Rs.${product.price}`;
 
-        // creating a heart button
-        const heartButton = document.createElement("button");
-        heartButton.classList.add("heartButton");
+    // creating a heart button
+    const heartButton = document.createElement("button");
+    heartButton.classList.add("heartButton");
 
-        // creating a heart button that'll come in effect when the button is hovered
-        const hoveredHeart = document.createElement("div");
-        hoveredHeart.classList.add("hoverHeart");
+    // creating a heart button that'll come in effect when the button is hovered
+    const hoveredHeart = document.createElement("div");
+    hoveredHeart.classList.add("hoverHeart");
 
-        // appending/inserting the name and price span to the text Section
-        tileText.appendChild(tileName );
-        tileText.appendChild(tilePrice);
+    // appending/inserting the name and price span to the text Section
+    tileText.appendChild(tileName );
+    tileText.appendChild(tilePrice);
 
-        // appending/inserting the hovered heart in the actual button
-        heartButton.appendChild(hoveredHeart);
+    // appending/inserting the hovered heart in the actual button
+    heartButton.appendChild(hoveredHeart);
 
-        // appending/inserting the text section and heart button into text section container
-        tileTextContainer.appendChild(tileText);
-        tileTextContainer.appendChild(heartButton);
+    // appending/inserting the text section and heart button into text section container
+    tileTextContainer.appendChild(tileText);
+    tileTextContainer.appendChild(heartButton);
 
-        // appending/inserting the tile image and tile text container into the tile
-        tile.appendChild( tileImage );
-        tile.appendChild( tileTextContainer );
+    // appending/inserting the tile image and tile text container into the tile
+    tile.appendChild( tileImage );
+    tile.appendChild( tileTextContainer );
 
-        // appending the child to the container
-        container.appendChild(tile);
+    // appending the child to the container
+    container.appendChild(tile);
 
 }
 
@@ -122,7 +122,6 @@ function generateTrending( product ){
 
 }
 
-
 function generateTile( sortedArray , section ){
 
     switch(section){
@@ -143,15 +142,9 @@ function generateTile( sortedArray , section ){
     }
 }
 
-
-
-
 // Use the function with a callback
 fetchArtProduct(artProduct => {
-    // Perform operations on artProduct here
-
-    console.log( artProduct.artProduct.imgSrc )
-
+    // sorting the datas on different biases and storing them as an array
     const sortedOnTrend = sortArray( artProduct.artProduct , 'trendRating' );
     const sortedOnfeatured = sortArray( artProduct.artProduct , 'featuredIndex' );
     const sortedOnPurchased = sortArray( artProduct.artProduct , 'numberOfPurchased' );
@@ -159,6 +152,4 @@ fetchArtProduct(artProduct => {
     generateTile( sortedOnfeatured , 'featured' );
     generateTile( sortedOnPurchased , 'topPurchased' );
     generateTile( sortedOnTrend , 'trending' );
-
-
 });
